9c708788ce2991da3165a36af479282f4f7418ad
Andrew <pimanac@users.noreply.github.com>
2016-12-07 22:58:38 -0500

    added listing support to Get-Post + -asjob

--- src/RedditSharp.PowerShell/Cmdlets/Posts/GetPost.cs
+++ src/RedditSharp.PowerShell/Cmdlets/Posts/GetPost.cs
@@ -1,20 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing;
 using System.Linq;
 using System.Management.Automation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using RedditSharp.Things;
 
 namespace RedditSharp.PowerShell.Cmdlets
 {
+    /// <summary>
+    /// <para type="synopsis">Returns a post or multiple posts from a subreddit</para>
+    /// <para type="description">By default limit to 1000 records.  Set -Limit to -1 for unlimited.</para>
+    /// <example>
+    ///    <code>Get-Post -Subreddit example</code>
+    /// </example>
+    /// <example>
+    ///    <code>Get-Subreddit "example" | Get-Post -Sort Hot</code>
+    /// </example>
+    /// </summary>
     [Cmdlet(VerbsCommon.Get,"Post")]
     [OutputType(typeof(Post))]
+    [OutputType((typeof(IEnumerable<Post>)))]
     public class GetPost : PSCmdlet
     {
-        [Parameter(Mandatory=true)]
+        /// <summary>
+        /// <para type="description">Fully qualified url to the reddit post</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "ByUri",Mandatory=false,HelpMessage = "Url to the reddit post.")]
         [ValidateNotNullOrEmpty]
         public string Uri { get; set; }
+
+        /// <summary>
+        /// <para type="description">Subreddit name</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "ByName",Mandatory = false,HelpMessage = "Subreddit name from which")]
+        [Alias("Sub")]
+        public string Subreddit { get; set; }
+
+        /// <summary>
+        /// <para type="description">The page to return.  Gilded is sorted by new.</para>
+        /// </summary>
+        [Parameter(Mandatory = true,HelpMessage = "Subreddit page.")]
+        [ValidateSet("New","Rising","Hot")]
+        [Alias("Sort")]
+        public string SortBy { get; set; }
+
+        /// <summary>
+        /// <para type="description">Limits the number of records to return.  Defaults to 100.  Set to -1 for no limit</para>
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "Limits the number of records to return.  Default 100.  Set to -1 for no limit.")]
+        public int Limit { get; set; }
+
+        /// <summary>
+        /// process as job
+        /// </summary>
+        [Parameter()]
+        public SwitchParameter AsJob
+        {
+            get { return asjob; }
+            set { asjob = value; }
+        }
+        private bool asjob;
+
+        [Parameter(
+             ParameterSetName = "ByInputObject",
+             Mandatory = true,
+             ValueFromPipeline = true,
+             HelpMessage = "Subreddit object")]
+        [ValidateNotNullOrEmpty]
+        public Subreddit InputObject
+        {
+            get { return inputObject; }
+            set { inputObject = value; }
+        }
+
+        private Subreddit inputObject;
+
+        private GetPostJob job;
+
+        public GetPost()
+        {
+            Limit = 100;
+        }
 
         protected override void BeginProcessing()
         {
@@ -25,16 +94,181 @@ namespace RedditSharp.PowerShell.Cmdlets
 
         protected override void ProcessRecord()
         {
-            try
+            switch (ParameterSetName)
             {
-                WriteObject(Session.Reddit.GetPost(new Uri(Uri)));
-            }
-            catch (Exception ex)
-            {
-                WriteError(new ErrorRecord(ex, "CantGetPost", ErrorCategory.InvalidOperation, Uri));
+                case "ByInputObject":
+                    ByInputObject();
+                    break;
+                case "ByUri":
+                    ByUri();
+                    break;
+                case "ByName":
+                    ByName();
+                    break;
+                default:
+                    break;
             }
         }
 
+        private void ByUri()
+        {
+            if (AsJob)
+            {
+                job = new GetPostJob
+                {
+                    Subreddit = null,
+                    Limit = Limit,
+                    Name = "Get-Post  -Uri" + Uri,
+                    Sort = null,
+                    Url = Uri
+                };
+                JobRepository.Add(job);
+                WriteObject(job);
+                ThreadPool.QueueUserWorkItem(o => job.ProcessJob());
+            }
+            else
+            {
+                try
+                {
+                    WriteObject(Session.Reddit.GetPost(new Uri(Uri)));
+                }
+                catch (Exception ex)
+                {
+                    WriteError(new ErrorRecord(ex, "CantGetPost", ErrorCategory.InvalidOperation, Uri));
+                }
+            }
+        }
 
+        private void ByInputObject()
+        {
+            if (AsJob)
+            {
+                job = new GetPostJob
+                {
+                    Subreddit = inputObject,
+                    Limit = Limit,
+                    Name = "Get-Post",
+                    Sort = SortBy
+                };
+                JobRepository.Add(job);
+                WriteObject(job);
+                ThreadPool.QueueUserWorkItem(o => job.ProcessJob());
+            }
+            else
+            {
+                try
+                {
+                    switch (SortBy.ToLower())
+                    {
+                        case "hot":
+                            WriteObject(InputObject.Hot.GetListing(Limit, 100));
+                            break;
+                        case "new":
+                            WriteObject(InputObject.New.GetListing(Limit, 100));
+                            break;
+                        case "controversial":
+                            // todo: add this later
+                            break;
+                        case "gilded":
+                            // todo: add this later
+                            break;
+                        case "rising":
+                            WriteObject(InputObject.Rising.GetListing(Limit, 100));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    WriteError(new ErrorRecord(ex, "CantGetPost", ErrorCategory.InvalidOperation, Uri));
+                }
+            }
+        }
+
+        private void ByName()
+        {
+            inputObject = GetSubreddit();
+            ByInputObject();
+        }
+
+        private Subreddit GetSubreddit()
+        {
+            if (Session.Cache.ContainsKey("xsub_" + Subreddit))
+                return Session.GetCacheItem<Subreddit>("xsub_" + Subreddit);
+            try
+            {
+                return Session.Reddit.GetSubreddit(Subreddit);
+            }
+            catch (Exception ex)
+            {
+                Session.Log.Error("Unable to get subreddit", ex);
+                throw ex;
+            }
+        }
+
+    }
+
+    internal class GetPostJob : Job
+    {
+        public override void StopJob()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string StatusMessage { get; }
+        public override bool HasMoreData { get; }
+        public override string Location { get; }
+
+        public Subreddit Subreddit { get; set; }
+        public int Limit { get; set; }
+
+        public string Sort { get; set; }
+
+        public string Url { get; set; }
+
+
+        internal void ProcessJob()
+        {
+            SetJobState(JobState.Running);
+
+            if (!String.IsNullOrEmpty(Sort))
+            {
+                switch (Sort.ToLower())
+                {
+                    case "hot":
+                        Output.Add(PSObject.AsPSObject(Subreddit.Hot.GetListing(Limit, 100)));
+                        break;
+                    case "new":
+                        Output.Add(PSObject.AsPSObject(Subreddit.New.GetListing(Limit, 100)));
+                        break;
+                    case "controversial":
+                        // todo: add this later
+                        break;
+                    case "gilded":
+                        // todo: add this later
+                        break;
+                    case "rising":
+                        Output.Add(PSObject.AsPSObject(Subreddit.Rising.GetListing(Limit, 100)));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (!String.IsNullOrEmpty(Url))
+            {
+                try
+                {
+                    Output.Add(PSObject.AsPSObject(Session.Reddit.GetPost(new Uri(Url))));
+                }
+                catch (Exception ex)
+                {
+                    Output.Add(PSObject.AsPSObject(new ErrorRecord(ex, "CantGetPost", ErrorCategory.InvalidOperation, Url)));
+                }
+            }
+
+            Output.Complete();
+            SetJobState(JobState.Completed);
+        }
     }
 }
